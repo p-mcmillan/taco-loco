b01ccf69127b2adfdbea921e84e1897140956946
Pierre <p.mcmillan@me.com>
2023-10-09 15:10:39 -0700

    bug-gone-now

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -199,7 +199,7 @@ const Menu = (props) => {
         <img
           src={onion01}
           alt="onion"
-          className=" right-[-500px] top-[1480px] z-0 object-contain absolute overflow-hidden	"
+          className=" right-[-500px] top-[1600px] z-0 object-contain absolute overflow-hidden	"
         />
 
         <div className="xl:hidden">
